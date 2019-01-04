40c47b1af23aa779d8c9946a5e3882cb39ecf866
Bruno Nascimento <bruno.nascimento@M2MRJ.LOCAL>
2019-01-04 17:52:08 -0200

    consulta, inclusao e exclusao - falta incluir outros campos

--- app/models/pessoa.js
+++ app/models/pessoa.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize')
 const pessoa = sequelize.define('pessoa',{
 
 	idPessoa: {type: Sequelize.INTEGER, field: 'id_pessoa', allowNull: false, autoIncrement: true, primaryKey: true},
-	nome: {type: Sequelize.STRING(50), field: 'nome',  required: true, allowNull: false}
+	nome: {type: Sequelize.STRING(50), field: 'nome',  required: true, allowNull: false},
+	ativo: {type: Sequelize.BOOLEAN, field: 'fl_ativo', allowNull: false}
 	
 }, {freezeTableName:true, timestamps:false});
 
--- app/routes/pessoa.js
+++ app/routes/pessoa.js
@@ -1,5 +1,7 @@
 const pessoa = require('../controllers/pessoa')
 
 module.exports = function(server) {	
-	server.get('/pessoa', pessoa.getAll)
+	server.get('/pessoa/:descricao?', pessoa.consulta)
+	server.post('/pessoa/incluir', pessoa.incluir)
+	server.delete('/pessoa/:nome?', pessoa.remove)
 }
